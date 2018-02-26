15ce08ccbcea56d97fa682f1fa5a050d6f43b3c5
Fai <ffung@xebia.com>
2018-02-26 16:06:16 +0100

    by default install docker, docker-compose, virtualenv and ansible

--- custom-script.sh
+++ custom-script.sh
@@ -7,3 +7,8 @@ set -eux
 #
 # For example:
 # yum install -y curl wget git tmux firefox xvfb
+
+systemctl start NetworkManager
+apt-get -y install docker.io docker-compose python-pip
+usermod -aG docker vagrant
+pip install --upgrade pip virtualenv 'ansible==2.4.3.0'
